36a157152596cf7ea5439235468f01811e471642
Sandra Bristiel <sandra.bristiel@tactfactory.com>
2016-05-26 09:43:55 +0200

    Fix the drag and drop : now working

--- src/SandraPokemonBundle/Controller/DefaultController.php
+++ src/SandraPokemonBundle/Controller/DefaultController.php
@@ -3,9 +3,11 @@
 namespace SandraPokemonBundle\Controller;
 
 use SandraPokemonBundle\Entity\TypeDePokemons;
+use SandraPokemonBundle\Form\SqlType;
 use SandraPokemonBundle\Form\TypeDePokemonsType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -20,15 +22,24 @@ class DefaultController extends Controller
     /**
      * @Route("/sql")
      */
-    public function sqlAction()
+    public function sqlAction(Request $request)
     {
-        return $this->render('SandraPokemonBundle:default:sql.html.twig');
+        $sqlForm = $this->createForm(SqlType::class);
+
+        if ($request) {
+            var_dump($sqlForm->get("sql"));
+        }
+
+        return $this->render('SandraPokemonBundle:default:sql.html.twig', array(
+        'sqlForm' => $sqlForm->createView(),
+    ));
+
     }
 
     /**
      * @Route("/creation")
      */
-    public function creationAction()
+    public function creationAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -37,6 +48,15 @@ class DefaultController extends Controller
 
         $zones = $em->getRepository('SandraPokemonBundle:Zones')->findAll();
         $types = $em->getRepository('SandraPokemonBundle:Types')->findAll();
+
+        $pokemonForm->handleRequest($request);
+
+        if ($pokemonForm->isSubmitted() && $pokemonForm->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('sandra_pokemon_default_index');
+        }
+
 
         return $this->render('SandraPokemonBundle:default:creation.html.twig', array(
             'pokemon' => $pokemon,
---
+++ src/SandraPokemonBundle/Form/SqlType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SandraPokemonBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SqlType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sql')
+        ;
+    }
+}
